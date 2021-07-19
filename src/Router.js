c42b24e2fcaaf93382b8aa7dddc4a58277767b0d
yusufdeepwork <yusuf.deepwork@gmail.com>
2021-07-19 09:16:02 +0300

    add Drawer navigator

--- src/Router.js
+++ src/Router.js
@@ -12,32 +12,41 @@ import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
 import Jobs from './screens/Jobs';
 import Detail from './screens/Detail';
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const JobStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: '#ef5350',
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen name="Jobs" component={Jobs} />
+      <Stack.Screen
+        name="Detail"
+        options={({route}) => ({title: route.params.name})}
+        component={Detail}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const Router = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerTitleStyle: {
-            color: '#ef5350',
-            fontWeight: 'bold',
-          },
-        }}>
-        <Stack.Screen name="Jobs" component={Jobs} />
-        <Stack.Screen
-          name="Detail"
-          options={({route}) => ({title: route.params.name})}
-          component={Detail}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator>
+        <Drawer.Screen name="Jobs" component={JobStack} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default Router;
